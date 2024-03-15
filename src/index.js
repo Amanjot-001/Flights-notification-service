c137baa12022c9475a45ada776a99cb9cbdae2b4
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-16 01:26:38 +0530

    added setup and ticket model

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const apiRoutes = require('./routes')
 
 const { ServerConfig, Logger } = require('./config');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
