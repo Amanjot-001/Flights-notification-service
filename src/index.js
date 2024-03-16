b2ed7cc8dbf5e0367ffca8c98a8398ae20dfb4b4
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-17 00:03:02 +0530

    added createTicket api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const app = express();
 const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes')
 
-const mailsender = require('./config/email-config');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,15 +10,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Sucessfully started the server on PORT: ${ServerConfig.PORT}`);
-    try {
-        const response = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'amanjotsingh2309@gmail.com',
-            subject: 'is this working',
-            text: 'yes'
-        })
-    } catch (error) {
-        console.log(error)
-    }
     Logger.info("Successfully started the server", "root", {});
 });
